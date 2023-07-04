e48ad4bd0f9cf079bd0cd449762efb8d3bfab0e5
Slonetech <108058202+Slonetech@users.noreply.github.com>
2023-07-04 20:25:05 +0300

    onscroll event

--- script.js
+++ script.js
@@ -6,3 +6,15 @@ menu.addEventListener('click', () => {
     menu.classList.toggle('fa-times'); // toggle class fa-times
     navbar.classList.toggle('active'); // toggle class active
 });
+
+window.addEventListener('scroll', () => {
+    if(window.scrollY > 0) {
+        document.querySelector('.header').classList.add('active');
+    }
+    else
+    {
+        document.querySelector('.header').classList.remove('active');
+    }
+    menu.classList.remove('fa-times'); // remove class fa-times
+    navbar.classList.remove('active'); // remove class active   
+});
