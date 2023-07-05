851ba65406b94b4c174880dec6bcd66c24bc8a2b
Slonetech <108058202+Slonetech@users.noreply.github.com>
2023-07-05 15:45:35 +0300

    front end complete with placeholder

--- script.js
+++ script.js
@@ -122,3 +122,33 @@ var swiper = new Swiper(".vehicles-slider", {
       },
     },
   });
+
+  var swiper = new Swiper(".reviews-slider", {
+    slidesPerView: 1,
+    spaceBetween: 20,
+    grabCursor: true,
+    centeredSlides:true,
+    loop: true,
+    autoplay: {
+        delay: 7500,
+        disableOnInteraction: false
+      },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    breakpoints: {
+      "@0.00": {
+        slidesPerView: 1,
+
+      },
+      "@0.75": {
+        slidesPerView: 2,
+
+      },
+      "@1.00": {
+        slidesPerView: 3,
+
+      },
+    },
+  });
